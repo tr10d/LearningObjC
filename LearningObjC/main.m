87e78d79bfbe53da4c6a60aca7ed2300b3d10caf
s_ser <s_ser@list.ru>
2021-06-16 21:14:40 +0300

    init

---
+++ LearningObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LearningObjC
+//
+//  Created by  Sergei on 16.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
